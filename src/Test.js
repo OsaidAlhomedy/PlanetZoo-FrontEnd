36adfff875965f2b6a44e5c73ecf3834e89effb2
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-14 14:11:22 +0300

    basic structure crerated

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Test.css";
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <p>The final project</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
