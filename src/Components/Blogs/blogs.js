d09ec01d6931fee294927b3e7c631b8b3ae27038
alaabaroud <alaabaroud783@gmail.com>
2021-08-14 20:53:25 +0300

    blog static finished

--- src/Components/Blogs/blogs.js
+++ src/Components/Blogs/blogs.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
@@ -7,15 +9,20 @@ class Blog extends React.Component {
     render () {
         return (
             <>
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
+            
+
+<Card className="bg-dark text-white">
+  <Card.Img src="https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image" />
+  <Card.ImgOverlay>
     <Card.Title>Blog 1</Card.Title>
     <Card.Text>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
     </Card.Text>
+    <Card.Text>Reading time : 10 min</Card.Text>
     <Button variant="primary">Read More!</Button>
-  </Card.Body>
+
+  </Card.ImgOverlay>
 </Card>
             </>
             
