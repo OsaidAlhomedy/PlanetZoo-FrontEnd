ba7eb543d6f96e1ecdd03b1eb0605571661bc7c4
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-08-15 23:22:57 +0300

    add hero imgs and cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,142 @@
 import React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import Carousel from 'react-bootstrap/Carousel'
+import {Card, CardGroup, } from 'react-bootstrap'
+
+import ghazal from '../assets/7b8d864e41342a71212d90f4294ea069.jpg'
+import banda from '../assets/banda.jpg'
+import tigers from '../assets/tigers.jpg'
+import bird from '../assets/bird.jpg'
+import horseimg from '../assets/depositphotos_21193779-stock-photo-gray-arabian-horse-gallops-on.jpg'
+import { Button } from "bootstrap";
+
+import { Route } from 'react-router-dom'
+
+
+
+
 class Home extends React.Component {
   render() {
     return (
       <>
-        <Form>
-          <Form.Group className="mb-5">
-            <Form.Select>
-              <option value="1">
-                which animal do you want to know about ?{" "}
-              </option>
-              <option value="1">lion </option>
-              <option value="2">tiger</option>
-              <option value="3">you</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label> textarea</Form.Label>
-            <Form.Control as="textarea" rows={4} />
-          </Form.Group>
-          <Button> EXPLORE </Button>
-        </Form>
+        <Carousel>
+        <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={tigers}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={ghazal}
+      alt="First slide"
+      width='50'
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={bird}
+      alt="First slide"
+      width='50'
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={horseimg}
+      alt="First slide"
+      width='50'
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banda}
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+<CardGroup>
+
+
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+      
+     
+  <Route render={({ history}) => (
+    <Button 
+      type='button'
+      onClick={() => { history.push('/blog') }}
+    >
+      Read more!
+    </Button>
+  )} />
+
+    </Card.Body>
+    <Card.Footer>
+      
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    </Card.Footer>
+  </Card>
+</CardGroup>
       </>
     );
   }
