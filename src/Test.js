d44332aba08d862480aac361a363ad61daa9b5ca
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-15 21:06:30 +0300

    finished the routing

--- src/Test.js
+++ src/Test.js
@@ -7,17 +7,15 @@ import "./Test.css";
 import { withAuth0 } from "@auth0/auth0-react";
 import loadingGIF from "./assets/loading.gif";
 import Header from "./components/Header";
-import Blog from "./components/blogs";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Blog from "./components/Blogs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Profile from "./components/Profile";
 // import Footer from "./components/Footer";
-import Home from "./Home";
-import Logout from "./components/Logout"
+import Home from "./components/Home";
 
 class Test extends Component {
   render() {
-    const { isLoading, error } = this.props.auth0;
-    const {isAuthenticated}=this.props.auth0;
+    const { isLoading, error, isAuthenticated } = this.props.auth0;
     return isLoading ? (
       <div className="text-center">
         <img className="loadingImg" src={loadingGIF} alt="loading" />
@@ -26,15 +24,24 @@ class Test extends Component {
       <h2>Oops... {error.message}</h2>
     ) : (
       <Router>
-        {/* <Login/> */}
         <Switch>
           <Route exact path="/">
-            {isAuthenticated ? <Blog addBlog={this.addBlog} /> : <Login />}
-
+            <Header />
+            <Home />
+            <Footer />
           </Route>
-
+          <Route exact path="/blog">
+            <Blog />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/donation"></Route>
+          <Route exact path="/login"></Route>
+          <Route exact path="/adoption"></Route>
+          <Route exact path="/giveAnimals"></Route>
+          <Route exact path="/aboutus"></Route>
         </Switch>
-
       </Router>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,62 @@
-import React from 'react';
+import React from "react";
 import { withAuth0 } from "@auth0/auth0-react";
-import {Navbar,Container,Nav,NavDropdown,Col,Row} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Navbar, Container, Nav, NavDropdown, Col, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import imgLogo from "../assets/logoRightText-01.png";
+import Logout from "./components/Logout";
 
 class Header extends React.Component {
-    render(){
-      const { user, isAuthenticated } = this.props.auth0;
+  render() {
+    const { user, isAuthenticated } = this.props.auth0;
 
-        return (
-<>
-  <Navbar bg="dark" variant="dark" className='py-4'>
-    <Container>
-    <Container>
-      <Navbar.Brand href="app.js">
-        <img
-          alt=""
-          src="https://trello.com/1/cards/6117ccdbc5aaea8ec9c03df9/attachments/6117cd9494bdc903adbbf27b/download/planetPets-01.png"
-          width="100"
-          height="100"
-          className="d-inline-block "
-        />
-      ZOO PLANET  
-      </Navbar.Brand>
-    </Container>
-    <Nav 
-      className="mr-auto my-2 my-lg-0">
-      <Nav.Link href="home" > Home  </Nav.Link>
-      <Nav.Link href="blog">Blog</Nav.Link>
-      <NavDropdown title="Adopt/Donate" id="basic-nav-dropdown">
-          <NavDropdown.Item href="Adopt an Animal">AdoptAnAnimal</NavDropdown.Item>
-          <NavDropdown.Item href="Contribute">Contribute</NavDropdown.Item>
-          <NavDropdown.Item href="Give Up Your Animal">GiveUpYourAnimal</NavDropdown.Item>
-        </NavDropdown>
+    return (
+      <>
+        <Navbar bg="dark" variant="dark" className="py-4">
+          <Container>
+            <Container>
+              <Navbar.Brand href="app.js">
+                <img
+                  alt="logo"
+                  src={imgLogo}
+                  width="100"
+                  height="100"
+                  className="d-inline-block "
+                />
+                ZOO PLANET
+              </Navbar.Brand>
+            </Container>
+            <Nav className="mr-auto my-2 my-lg-0">
+              <Nav.Link href="home"> Home </Nav.Link>
+              <Nav.Link href="blog">Blog</Nav.Link>
+              <NavDropdown title="Adopt/Donate" id="basic-nav-dropdown">
+                <NavDropdown.Item href="adoption">
+                  AdoptAnAnimal
+                </NavDropdown.Item>
+                <NavDropdown.Item href="donation">Contribute</NavDropdown.Item>
+                <NavDropdown.Item href="giveAnimals">
+                  GiveUpYourAnimal
+                </NavDropdown.Item>
+              </NavDropdown>
 
-        <NavDropdown  title= {isAuthenticated ?  `Hello ${user.name}` : `Hello Guest `} id="basic-nav-dropdown">
-    
-        <NavDropdown.Item href="profile">
-          
-          Profile
-          </NavDropdown.Item>
-          <NavDropdown.Item href="login">
-          
-          Login
-          </NavDropdown.Item>
-          
-        </NavDropdown>
-
-    </Nav>
-    </Container>
-  </Navbar>
-</>
-        )
-    }
+              <NavDropdown
+                title={isAuthenticated ? `Hello ${user.name}` : `Hello Guest `}
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="profile">Profile</NavDropdown.Item>
+                {isAuthenticated ? (
+                  <NavDropdown.Item>
+                    <Logout />
+                  </NavDropdown.Item>
+                ) : (
+                  <NavDropdown.Item href="login">Login</NavDropdown.Item>
+                )}
+              </NavDropdown>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
 }
 
 export default withAuth0(Header);
