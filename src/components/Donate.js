fe705af12dd047f0c118293471efb1d2f9758add
alaabaroud <alaabaroud783@gmail.com>
2021-08-16 22:45:49 +0300

    finished with donate -no css

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -8,17 +8,23 @@ export class Donate extends Component {
         <Row className = "text-center mt-5">
 
           <h1 > Donation page </h1>
-
+<p> A donor service representative with the American Society for the Prevention of Cruelty to Animals, or ASPCA, said 80 percent of donations goes directly to help animals. Help us to stop Animal's Suffering and Donate Here</p>
 
 
         </Row>
-      <Row className = 'mt-5'>
+      <Row className = "mt-5 mr-4">
 <Col>
       
 <iframe title = 'donation' src="https://donorbox.org/embed/planetzoo?designation=General%20Care&default_interval=o&hide_donation_meter=true" name="donorbox" allowpaymentrequest="" seamless="seamless" frameBorder="0" scrolling="no" height="900px" width="100%" style={{'max-width': '500px', 'min-width': '250p', 'max-height':'none!important'}}></iframe>
 </Col>
 <Col>
-<p> ljfiehfiuwhefoieh</p>
+<h3> Why People Donate:</h3>
+<p>Donating to the causes you care about not only benefits the charities themselves, it can be deeply rewarding for you too. Millions of people give to charity on a regular basis to support causes they believe in, as well as for the positive effect it has on their own lives.
+</p>
+<p> Many people are concerned that their donations to charity may be reduced by tax or administrative costs, preventing the full amount from reaching the people or causes they really want to help. Thankfully because of the low fees, you can make sure that you gave the most of every donation to charity.</p>
+<p>
+  Also, Your own charitable donations can inspire your nearest and dearest to give to causes important to them, and could even bring about a family-wide effort to back a charity or charities that have special significance to you as a group.
+</p>
 </Col>
 </Row>
       </ Container>
