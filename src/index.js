cb007d124325831c9f83718e4b3fe01911fd99c3
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-19 15:37:45 +0300

    fixed the url

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import "./index.css";
 import Test from "./Test";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-
 ReactDOM.render(
   <Auth0Provider
     domain="dev-gyy9agxm.us.auth0.com"
     clientId="ZNFvUYh52Fi74H2ROSbMi1mdSpUN6FAL"
-    redirectUri={process.env.REACT_APP_URL}
+    redirectUri={window.location.origin}
   >
     {/* <App /> */}
     <Test />
