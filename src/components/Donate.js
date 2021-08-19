afd280cefaa21a7e93a054ce2559e82f26cf68da
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-19 15:33:20 +0300

    fixed a bug in the donation page

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import { Container, Row, Col, Image, Card } from "react-bootstrap";
 import "./Donate.css";
 // import DonatePic from '../assets/donateanimals.png'
-
-import streetCat from "../assets/headCat.jpg";
+import streetCat from "../assets/donationHero-01-01.png";
 import donate1 from "../assets/donation-01.png";
 import cat from "../assets/onur-binay-E18nZ_OHh04-unsplash.jpg";
 
@@ -12,65 +11,11 @@ export class Donate extends Component {
     return (
       <Container className="border" fluid>
         <Row className="text-center">
-          <h1> Donation page ! </h1>
-
-          <p className="paragraph">
-            {" "}
-            In PlanetZoo 90% of donations goes directly to help animals. Help us
-            to stop Animal's Suffering and Donate Here:{" "}
-          </p>
-
-          <Image src={streetCat} alt="poster" fluid />
-
           <Image src={streetCat} alt="poster" fluid />
         </Row>
-        <Row className="mt-5 mr-4 p-5">
-          <Col>
-            <iframe
-              title="donation"
-              src="https://donorbox.org/embed/planetzoo?designation=General%20Care&default_interval=o&hide_donation_meter=true"
-              name="donorbox"
-              allowpaymentrequest=""
-              seamless="seamless"
-              frameBorder="0"
-              scrolling="no"
-              height="900px"
-              width="100%"
-              style={{
-                "max-width": "500px",
-                "min-width": "250p",
-                "max-height": "none!important",
-              }}
-            ></iframe>
-          </Col>
-          <Col>
-            <h3> Why People Donate:</h3>
-            <p className="para">
-              Donating to the causes you care about not only benefits the
-              charities themselves, it can be deeply rewarding for you too.
-              Millions of people give to charity on a regular basis to support
-              causes they believe in, as well as for the positive effect it has
-              on their own lives.
-            </p>
-            <p className="para">
-              {" "}
-              Many people are concerned that their donations to charity may be
-              reduced by tax or administrative costs, preventing the full amount
-              from reaching the people or causes they really want to help.
-              Thankfully because of the low fees, you can make sure that you
-              gave the most of every donation to charity.
-            </p>
-            <p className="para">
-              Also, Your own charitable donations can inspire your nearest and
-              dearest to give to causes important to them, and could even bring
-              about a family-wide effort to back a charity or charities that
-              have special significance to you as a group.
-            </p>
-          </Col>
-        </Row>
 
-        <Row className="mt-2 mr-4 p-2">
-          <Col>
+        <Row className="mt-5 mr-4 p-2">
+          <Col className="d-flex justify-content-center">
             <iframe
               title="donation"
               src="https://donorbox.org/embed/planetzoo?designation=General%20Care&default_interval=o&hide_donation_meter=true"
@@ -134,8 +79,9 @@ export class Donate extends Component {
           </Col>
         </Row>
 
-        <Row className="dividerRow2"></Row>
-        <Row>
+        <Row className="dividerRow2" style={{ height: "200px" }}></Row>
+
+        <Row className="mt-3">
           <Col>
             <h3 className="para">OTHER DONATION OPTIONS</h3>
             <br></br>
@@ -169,8 +115,86 @@ export class Donate extends Component {
           <Row className="p-5">
             <Image src={donate1} fluid />
           </Row>
+          {/* <Card class="card text-white bg-primary mb-3" style={{ width: '18rem' }}>
+<Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title> Card Title </Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card> */}
         </Row>
       </Container>
+
+      // <div>
+      //   <Card style={{ width: "20rem", textAlign: "center" }}>
+      //     <Card.Body>
+      //       <Tabs
+      //         defaultActiveKey="profile"
+      //         id="uncontrolled-tab-example"
+      //         className="mb-3"
+      //       >
+      //         <Tab eventKey="onTime" title="One Time">
+      //           <ButtonGroup aria-label="Basic example">
+      //             <Button variant="secondary">10$</Button>
+      //             <Button variant="secondary">20$</Button>
+      //             <Button variant="secondary">50$</Button>
+      //           </ButtonGroup>
+      //           <Form>
+      //             <Form.Group className="mb-3" controlId="formBasicEmail">
+      //               <Form.Label>Email address</Form.Label>
+      //               <Form.Control type="email" placeholder="Enter email" />
+      //               <Form.Text className="text-muted">
+      //                 Add your Email here!
+      //               </Form.Text>
+      //             </Form.Group>
+
+      //             <Form.Group className="mb-3" controlId="formBasicPassword">
+      //               <Form.Label>Password</Form.Label>
+      //               <Form.Control type="password" placeholder="Password" />
+      //             </Form.Group>
+      //             <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      //               <Form.Check type="checkbox" label="Check me out" />
+      //             </Form.Group>
+      //             <Button variant="primary" type="submit">
+      //             Via PayPal
+      //             </Button>
+      //           </Form>
+      //         </Tab>
+      //         <Tab eventKey="Monthly" title="Monthly">
+      //         <ButtonGroup aria-label="Basic example">
+      //             <Button variant="secondary">10$</Button>
+      //             <Button variant="secondary">20$</Button>
+      //             <Button variant="secondary">50$</Button>
+      //           </ButtonGroup>
+      //           <Form>
+      //             <Form.Group className="mb-3" controlId="formBasicEmail">
+      //               <Form.Label>Email address</Form.Label>
+      //               <Form.Control type="email" placeholder="Enter email" />
+      //               <Form.Text className="text-muted">
+      //                 We'll never share your email with anyone else.
+      //               </Form.Text>
+      //             </Form.Group>
+
+      //             <Form.Group className="mb-3" controlId="formBasicPassword">
+      //               <Form.Label>Password</Form.Label>
+      //               <Form.Control type="password" placeholder="Password" />
+      //             </Form.Group>
+      //             <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      //               <Form.Check type="checkbox" label="Check me out" />
+      //             </Form.Group>
+      //             <Button variant="primary" type="submit">
+      //                Via PayPal
+      //             </Button>
+      //           </Form>
+      //         </Tab>
+      //       </Tabs>
+      //       {/* <Button variant="primary">Go somewhere</Button> */}
+      //     </Card.Body>
+      //   </Card>
+      // </div>
     );
   }
 }
